b5945b7bf1b1b66d66d574b33b49e747ed5fe9c8
nishatislam04 <nishatislam3108@gmail.com>
2025-04-20 02:25:11 +0600

    add: some config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	devIndicators: false,
+	watchOptions: {
+		ignored: ["**/.git/**", "**/.next/**", "**/node_modules/**"],
+	},
 };
 
 export default nextConfig;
