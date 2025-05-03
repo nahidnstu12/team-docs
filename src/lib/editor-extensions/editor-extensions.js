0062ef5334e18a5a3e0050465f5def6e3f96cc39
Minhajul Islam <nishatislam3108@gmail.com>
2025-05-04 01:47:39 +0600

    update old control css selector

--- src/lib/editor-extensions/editor-extensions.js
+++ src/lib/editor-extensions/editor-extensions.js
@@ -1,9 +1,9 @@
 import StarterKit from "@tiptap/starter-kit";
 import TaskList from "@tiptap/extension-task-list";
-import { ColorExtensions } from "@/components/editor/extensions/colors";
 import TaskItem from "@tiptap/extension-task-item";
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import { all, createLowlight } from "lowlight";
+import { ColorExtensions } from "@/components/editor/extensions/color";
 
 const lowlight = createLowlight(all);
 
@@ -11,17 +11,14 @@ export const editorExtensions = [
 	StarterKit.configure({
 		history: true,
 	}),
-	// CustomHeading,
 	TaskList.configure({
 		nested: true,
 	}),
 	TaskItem.configure({
 		nested: true,
 	}),
-
 	CodeBlockLowlight.configure({
 		lowlight,
 	}),
-
 	...ColorExtensions,
 ];
