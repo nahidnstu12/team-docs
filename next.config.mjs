c3e2df39b1877cfa84b77de3a9cb2dcbe8c1ad89
Minhajul Islam <nishatislam3108@gmail.com>
2025-05-15 09:56:01 +0600

    fix: verecel rhel issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	devIndicators: false,
 	experimental: {
 		authInterrupts: true,
+	},
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			config.plugins = [...config.plugins, new PrismaPlugin()];
+		}
+
+		return config;
 	},
 };
 
