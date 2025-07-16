532ffeca7c62d2bae7c15d4b8cdf3b55aac830cf
minhajul islam <nishatislam3108@gamil.com>
2025-07-17 02:39:00 +0600

    update nextjs canary to latest && move some dep to devDep & update nex.conf

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,20 @@ const nextConfig = {
   experimental: {
     authInterrupts: true,
     ppr: "incremental",
+    // Forward browser logs to the terminal for easier debugging
+    browserDebugInfoInTerminal: true,
+
+    // Enable new caching and pre-rendering behavior
+    dynamicIO: true, // will be renamed to cacheComponents in Next.js 16
+
+    // Activate new client-side router improvements
+    clientSegmentCache: true,
+
+    // Explore route composition and segment overrides via DevTools
+    devtoolSegmentExplorer: true,
+
+    // Enable persistent caching for the turbopack dev server and build.
+    turbopackPersistentCaching: true,
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
